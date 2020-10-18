5c02e90d8e477f16787803f4a1d5ffa336e92fea
unknown <levinas.al2@gmail.com>
2020-10-18 08:39:35 +0300

    changed in feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>This is a change to be save as a Feature!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
